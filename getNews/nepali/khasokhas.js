faaf220754b74263977cd74cc6593ca28d9954be
Sajjal Neupane <sajjalco@gmail.com>
2021-11-05 20:09:35 -0500

    Update Nepali news sources

--- getNews/nepali/khasokhas.js
+++ getNews/nepali/khasokhas.js
@@ -5,37 +5,38 @@ const url = "https://www.khasokhas.com/";
 
 //Get Title and URL
 function khasokhas() {
-  let data = [];
-  return fetch(`${url}`)
-    .then((response) => response.text())
-    .then((body) => {
-      const $ = cheerio.load(body);
-      $(".custom-list-item a").each(function (i, element) {
-        const $element = $(element);
-        let title = $element.text();
-        let link = $element.attr("href");
-        title = title.replace(/\s+/g, " ").trim();
-        data.push({ source: "Khasokhas", news: { title, link: link } });
-      });
-      return data;
-    });
+    let data = [];
+    return fetch(`${url}`)
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+            $(".custom-list-item a").each(function(i, element) {
+                const $element = $(element);
+                let title = $element.text();
+                let link = $element.attr("href");
+                title = title.replace(/\s+/g, " ").trim();
+                if (title.length > 5)
+                    data.push({ source: "Khasokhas", news: { title, link: link } });
+            });
+            return data;
+        });
 }
 
 //Get the full News
 function khasokhasFull(newsUrl) {
-  let data = [];
-  return fetch(`${newsUrl}`)
-    .then((response) => response.text())
-    .then((body) => {
-      const $ = cheerio.load(body);
-      //getting the news
-      $(".post-entry p").each(function (i, element) {
-        const $element = $(element);
-        let news = $element.text();
-        news = news.replace(/\s+/g, " ").trim();
-        data.push(news);
-      });
-      return data;
-    });
+    let data = [];
+    return fetch(`${newsUrl}`)
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+            //getting the news
+            $(".post-entry p").each(function(i, element) {
+                const $element = $(element);
+                let news = $element.text();
+                news = news.replace(/\s+/g, " ").trim();
+                data.push(news);
+            });
+            return data;
+        });
 }
 module.exports = { khasokhas, khasokhasFull };
--- getNews/nepali/onlineKhabar.js
+++ getNews/nepali/onlineKhabar.js
@@ -5,43 +5,44 @@ const url = "https://www.onlineKhabar.com/";
 
 //Get Title and URL
 function onlineKhabar() {
-  let data = [];
-  return fetch(`${url}`)
-    .then((response) => response.text())
-    .then((body) => {
-      const $ = cheerio.load(body);
-      $(".most_recomenede_side a")
-        .slice(0, 13)
-        .each(function (i, element) {
-          const $element = $(element);
-          let title = $element.text();
-          let link = $element.attr("href");
-          title = title.replace(/\s+/g, " ").trim();
-          if (title.length > 5)
-            data.push({ source: "OnlineKhabar", news: { title, link: link } });
+    let data = [];
+    return fetch(`${url}`)
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+            $(".ok-news-post.ok-post-rtl a")
+                .slice(0, 8)
+                .each(function(i, element) {
+                    const $element = $(element);
+                    let title = $element.text();
+                    let link = $element.attr("href");
+                    title = title.replace(/\s+/g, " ").trim().split(' ').slice(1).join(' ');
+                    if (title.length > 5)
+                        data.push({ source: "OnlineKhabar", news: { title, link: link } });
+                });
+            return data;
         });
-      return data;
-    });
 }
 
 //Get the full News
 function onlineKhabarFull(newsUrl) {
-  let data = [];
-  return fetch(`${newsUrl}`)
-    .then((response) => response.text())
-    .then((body) => {
-      const $ = cheerio.load(body);
-      //getting the news
-      $(
-        ".col.colspan3.main__read--content.ok18-single-post-content-wrap p"
-      ).each(function (i, element) {
-        const $element = $(element);
-        let news = $element.text();
-        news = news.replace(/\s+/g, " ").trim();
-        data.push(news);
-      });
-      return data;
-    });
+    console.log(newsUrl);
+    let data = [];
+    return fetch(`${newsUrl}`)
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+            //getting the news
+            $(
+                ".ok18-single-post-content-wrap p"
+            ).each(function(i, element) {
+                const $element = $(element);
+                let news = $element.text();
+                news = news.replace(/\s+/g, " ").trim();
+                data.push(news);
+            });
+            return data;
+        });
 }
 
 module.exports = { onlineKhabar, onlineKhabarFull };

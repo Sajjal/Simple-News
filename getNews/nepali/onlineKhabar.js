46dfb235a2909adc63092c9845968b12a7850cc5
Sajjal Neupane <https://mrsajjal.com>
2022-03-23 19:35:58 -0500

    Update OnlineKhabar

--- getNews/nepali/onlineKhabar.js
+++ getNews/nepali/onlineKhabar.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
-const url = "https://www.onlineKhabar.com/";
+const url = "https://www.onlinekhabar.com/wp-json/okapi/v1/trending-posts?limit=9";
 
 //Get Title and URL
 function onlineKhabar() {
@@ -9,24 +9,18 @@ function onlineKhabar() {
     return fetch(`${url}`)
         .then((response) => response.text())
         .then((body) => {
-            const $ = cheerio.load(body);
-            $(".ok-news-post.ok-post-rtl a")
-                .slice(0, 8)
-                .each(function(i, element) {
-                    const $element = $(element);
-                    let title = $element.text();
-                    let link = $element.attr("href");
-                    title = title.replace(/\s+/g, " ").trim().split(' ').slice(1).join(' ');
-                    if (title.length > 5)
-                        data.push({ source: "OnlineKhabar", news: { title, link: link } });
-                });
+            body = JSON.parse(body)
+            body = body.data.news
+            for (let i = 0; i < body.length; i++) {
+                if (data.length < 8 && body[i].title.length > 5)
+                    data.push({ source: "OnlineKhabar", news: { title: body[i].title, link: body[i].link } });
+            }
             return data;
         });
 }
 
 //Get the full News
 function onlineKhabarFull(newsUrl) {
-    console.log(newsUrl);
     let data = [];
     return fetch(`${newsUrl}`)
         .then((response) => response.text())
